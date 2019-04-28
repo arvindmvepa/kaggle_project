e0c37a69fcdfc12431a69a0641de157d35b723e5
arvind <arvind.m.vepa@gmail.com>
2019-04-28 12:38:28 -0700

    bug

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from exp.run import run_experiment_script
 
 if __name__ == '__main__':
     # run_experiment_script(params="test.yml", num_searches=2, n_fold=2)
-    # run_experiment_script(params="lgb.yml", num_searches=20, n_fold=10)
-    # run_experiment_script(params="xgb.yml", num_searches=20, n_fold=10)
+    # run_experiment_script(params="lgb.yml", save_results="exp3.csv", num_searches=20, n_fold=10)
+    # run_experiment_script(params="xgb.yml", save_results="exp3.csv", num_searches=20, n_fold=10)
     run_experiment_script(params="cat.yml", save_results="exp3.csv", num_searches=20, n_fold=10)
