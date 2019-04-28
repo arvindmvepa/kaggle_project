6a3a330759ed153d49ceb2c60c9a1ef3713bbe65
arvind <arvind.m.vepa@gmail.com>
2019-04-28 11:57:37 -0700

    experiment script for optimizing on the cloud

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from exp.run import run_experiment_script
 
 if __name__ == '__main__':
-    run_experiment_script(params="test.yml", num_searches=2, n_fold=2)
+    # run_experiment_script(params="test.yml", num_searches=2, n_fold=2)
+    # run_experiment_script(params="lgb.yml", num_searches=20, n_fold=10)
+    # run_experiment_script(params="xgb.yml", num_searches=20, n_fold=10)
+    run_experiment_script(params="cat.yml", num_searches=20, n_fold=10)
