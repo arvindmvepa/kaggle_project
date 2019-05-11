e4db73a55083b9f63ff3db6c50b1ed8c26614679
arvind <arvind.m.vepa@gmail.com>
2019-05-11 06:07:53 -0700

    bug

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     # run_experiment_script(params="xgb.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
     # run_experiment_script(params="cat.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
     run_experiment_script(params="lgb_es.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
-    run_experiment_script(params="xgb.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
-    run_experiment_script(params="cat.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
+    run_experiment_script(params="xgb_es.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
+    run_experiment_script(params="cat_es.yml", save_results="exp3_1.csv", num_searches=40, n_fold=10)
